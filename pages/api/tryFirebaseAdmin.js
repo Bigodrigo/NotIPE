eb18c860b2b37d824af2bdd5277f3fcfa36ad0cd
Bigodrigo <roesbo1@hotmail.com>
2023-02-17 09:52:51 -0300

    Ajustes Kauan

--- pages/api/tryFirebaseAdmin.js
+++ pages/api/tryFirebaseAdmin.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     //const token = documentSnapshot.data()[FCM_TOKEN_KEY];
     const message = 'Testezinho sem o jão!'
     const payload = {
-        token: "fsMJvirtSueLF-_1DP-97I:APA91bHKZq_3xF6iNbqfkCZp6mumtIeQYwI9-byKJWiopdvP0No9PuUWuFzwotW4EamUxwFgM-l28rBEcJAjoaZFxJsg-uk4upGCackPvP_rkwbs_zTHYmx-1t1LoVsZSFmTLB6qL1nr",
+        token: "fhU2rIxPQuO7IuOzPavYUU:APA91bHA1TY6fEnyrCgE780RbF4UTYAcdGn7UYzl9H_OTDO5lhA8w0MlsRoNUEWw-2LJF1kCbYyzhhV3TjFu0yuk8tnr3wGmphuN2dcTzExTAFm3w0CnAArF4V1WSDsAMJezXSCTMJEq",
         //token,
         notification: {
             title: 'Confia!',
