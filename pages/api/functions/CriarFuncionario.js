d5b038e21f3ba1907a4563af49e76b8b35818153
Kauan Voltz Fagundes <kauan-fagundes@IPESAUDE.INTRA.RS.GOV.BR>
2023-03-03 12:02:11 -0300

    validação da tela de sign up feito (teoricamente)

--- pages/api/functions/CriarFuncionario.js
+++ pages/api/functions/CriarFuncionario.js
@@ -8,32 +8,35 @@ export default async function handler(req, res) {
     let senha = req.body.senha
     let nome = req.body.nome
     let cargo = req.body.cargo
-
-    try{
-        if(req.method === 'POST'){
+    try {
+        if (req.method === 'POST') {
             let snapshot =
-             await admin.auth()
-             .createUser({
-                email:email,
-                emailVerified:true,
-                password:senha
+                await admin.auth()
+                    .createUser({
+                        email: email,
+                        emailVerified: true,
+                        password: senha
 
-             })
-             .then((UserRecord)=>{
-                const data = {
-                    nome:nome,
-                    email:email,
-                    cargo:cargo
-                }
-                 admin.firestore()
-                 .collection('funcionarios')
-                 .doc(UserRecord.uid)
-                 .set(data)
-             })
+                    })
+                    .then((UserRecord) => {
+                        const data = {
+                            nome: nome,
+                            email: email,
+                            cargo: cargo
+                        }
+                        admin.firestore()
+                            .collection('funcionarios')
+                            .doc(UserRecord.uid)
+                            .set(data)
+                            .then(() => {
+                                res.status(200).json('teste')
+                            })
+                    })
 
         }
-    }catch(e){
-        console.log("deu ruim " , e.code)
+    } catch (e) {
+        res.status(400).json(e.code)
+        console.log("deu ruim ", e.code)
     }
 
 }
